670a24a060c6136cb60c907dde8995a1a0b3dc94
abdul <abdul@DESKTOP-U0I9PGQ>
2023-10-05 16:56:02 +0300

    Editing My code

--- AMA/src/LinkedList.java
+++ AMA/src/LinkedList.java
@@ -3,6 +3,11 @@ class Node<T> {
 	
 	T data;
 	Node next;
+	
+	public Node(T d) {
+		data=d;
+		next=null;
+	}
 }
 
 
@@ -12,69 +17,69 @@ public class LinkedList<T> {
 	Node<T> head;
 	Node<T> current;
 	
-public boolean isempty() {     //Checking if the list is empty
-		
-		return head == null;
-	}
-	
-	public boolean islast() {  //Checking if the node is the last one
-		
-		return current.next == null;
-	}
-	public void findfirst() { // making the current point on the first node
-		
-		current = head;
-	}
-	
-	public void findnext() { // move the current to the next node
-		
-		current = current.next;
-	}
-	
-	public T retrieve () {  // obtain the current node
-		
-		return current.data;
-	}
-	
-	public void update (T val) { // updating the node
-		
-		current.data = val;
-	}
-	
-    public void insert (T val) { // inserting nodes
-		
-		Node<T> tmp;
-		
-		if (isempty())
-			head = current = newNode <T> (val);
-		
-		else {
-			
-			tmp = current.next;
-			current.next = newNode<T> (val)
-			current = current.next;
-			current.next = tmp;
-		}
-	}
-	 
-    public void remove() { // removing the current node
-    	
-    	if (current == head)
-    		head = null
-    		
-    		else {
-    			
-    			Node <T> tmp = head;
-    			
-    			while(tmp.next != current)
-    				tmp = tmp.next
-    				
-    				tmp.next = current.next;
-    		}
-    	if (current.next == null)
-    		current = head;
-    	else
-    		current = current.next;
+	public boolean isEmpty() {     //Checking if the list is empty
+
+        return head == null;
     }
+
+    public boolean isLast() {  //Checking if the node is the last one
+
+        return current.next == null;
+    }
+    public void findFirst() { // making the current point on the first node
+
+        current = head;
+    }
+
+    public void findNext() { // move the current to the next node
+
+        current = current.next;
+    }
+
+    public T retrieve () {  // obtain the current node
+
+        return current.data;
+    }
+
+    public void update (T val) { // updating the node
+
+        current.data = val;
+    }
+
+    public void insert (T val) { // inserting nodes
+
+        Node<T> tmp;
+
+        if (isEmpty())
+             head=current=new Node<T>(val);
+        else {
+
+            tmp = current.next;
+            current.next = new Node <T> (val);
+            current = current.next;
+            current.next = tmp;
+        }
+    }
+
+    public void remove() { // removing the current node
+
+        if (current == head)
+            head = null;
+
+            else {
+
+                Node <T> tmp = head;
+
+                while(tmp.next != current)
+                    tmp = tmp.next;
+
+                    tmp.next = current.next;
+            }
+        if (current.next == null)
+            current = head;
+        else
+            current = current.next;
+    }
+
 
 }
